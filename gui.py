08606dbf3b8c775647dfbaf8bfc09b82f1c72f3e
Azizbek <a298b112@gmail.com>
2023-10-01 13:09:03 +0200

    Addded a robot icon

--- gui.py
+++ gui.py
@@ -5,6 +5,6 @@ label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Write a todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My to-do app", layout=[[label], [input_box, add_button]])
+window = sg.Window("My to-do app", layout=[[label], [input_box, add_button]], icon="bot.ico")
 window.read()
 window.close()
